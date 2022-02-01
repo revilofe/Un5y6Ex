e79b02df145e94c6daf1fa92ab5f47395bdfd93e
Edu <revilofe@gmail.com>
2022-02-01 21:42:44 +0100

    Final MonoConcierto

--- src/main/kotlin/Piano.kt
+++ src/main/kotlin/Piano.kt
@@ -1,31 +1,29 @@
-//Un piano es un instrumento que interpreta las notas con un timbre muy
-//característico
-class Piano  //podenos añadir tantos atributos y métodos como necesitemos
-    : Instrumento() {
-    //implementamos el método abstracto
+
+//Un piano es un instrumento que interpreta las notas con un timbre muy característico
+class Piano{
+
+   //tabla que almacena las notas a interpretar
+    private var melodia = mutableListOf<Nota>()
+
+    fun incorporaNota(nota: Nota) {
+        melodia.add(nota)
+    }
+
     //Recorreremos las notas y las interpretaremos de la forma específica del piano.
-    override fun interpretar() {
-        for (nota in melodia) {
+    fun play() {
+        println("Tocando piano")
+        melodia.forEach { nota ->
             when (nota) {
-                Nota.DO -> print("do ")
-                Nota.RE -> print("re ")
-                Nota.MI -> print("mi ")
-                Nota.FA -> print("fa ")
-                Nota.SOL -> print("sol ")
-                Nota.LA -> print("la ")
-                Nota.SI -> print("si ")
+                Nota.DO -> print("dooo ")
+                Nota.RE -> print("reee ")
+                Nota.MI -> print("miiii ")
+                Nota.FA -> print("faaa ")
+                Nota.SOL -> print("soool ")
+                Nota.LA -> print("laaa ")
+                Nota.SI -> print("siiii ")
+                else -> {"ERROR"}
             }
         }
         println("")
     }
-}
-
-//Programa Principal
-fun main(args: Array<String>) {
-    val cancion = arrayOf(Nota.DO, Nota.SI, Nota.SOL, Nota.RE, Nota.FA) //notas
-    val p = Piano()
-    for (nota in cancion) { //añadimos las notas al piano
-        p.add(nota)
-    }
-    p.interpretar()
 }
