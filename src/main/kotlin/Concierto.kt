9dfaf2a790b0bd402afb9b480583fd94b0011890
Edu <revilofe@gmail.com>
2022-02-02 12:07:11 +0100

    Tercera versión final.

--- src/main/kotlin/Concierto.kt
+++ src/main/kotlin/Concierto.kt
@@ -3,6 +3,6 @@
 fun main() {
     val pianista = Pianista()
     val cancion = arrayOf<Nota?>(Nota.DO, Nota.FA, Nota.FA, Nota.MI)
-    pianista.cancion = cancion
+    pianista.partitura = cancion
     pianista.interpretar()
 }
--- src/main/kotlin/Pianista.kt
+++ src/main/kotlin/Pianista.kt
@@ -2,17 +2,17 @@
 class Pianista(unaCancion: Array<Nota?> = arrayOfNulls<Nota?>(0)) {
 
     private val piano = Piano()
-    var cancion: Array<Nota?> = unaCancion
+    var partitura: Array<Nota?> = unaCancion
         set(value) {
             field = value
             value.forEach { nota ->
-                i("Pianista.setCancion","incorpora nota $nota de canción")
+                i("Pianista.setPartitura","incorpora nota $nota de canción")
                 nota?.let { piano.incorporaNota(nota) }
             }
         }
 
     init {
-        cancion = unaCancion
+        partitura = unaCancion
     }
 
     fun interpretar() = piano.play()
